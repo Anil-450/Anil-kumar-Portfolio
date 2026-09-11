cbaad23d3960eddc7ea2976d67e91d85f7041309
Anil-450 <anilvuyyuru26@gmail.com>
2026-09-11 11:19:17 +0530

    Add scroll event listener for navigation highlighting

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Welcome to Anil Kumar's Portfolio!");
+
+
+// Highlight navigation while scrolling
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop &&
+            scrollY < sectionTop + sectionHeight) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
